740e2353c7ff4a2c373ad9df423a3bee934f4a89
sfrank <sascha.frank.88@gmx.de>
2022-01-06 04:11:44 +0100

    Update install_missing_pkg_apt.sh
    - -a added function to output log-file content (last x lines)

--- install_missing_pkg_apt.sh
+++ install_missing_pkg_apt.sh
@@ -13,35 +13,64 @@ function check_pkg_installed {
     echo $result_
 }
 
+
+LOG_FILE="/var/log/missing_apt.log"
+
+ERRORS_COUNT=0
+ACTION_POST_LOG_AFTER=0
+
 if [[ $# -gt 1 ]]; then
     echo "> getting missing packages"
     declare -a myNotInstalledPkgs=()
     count=0
     for package in "$@"
     do 
-        result_=$(check_pkg_installed $package)
-        if [[ $result_ -ne 0 ]]; then
-            myNotInstalledPkgs+=("${package}")
+        if [[ $package =~ "-a" ]];then 
+            echo "option:output log after"
+            ACTION_POST_LOG_AFTER=1
+        else
+            echo $package
+            result_=$(check_pkg_installed $package)
+            if [[ $result_ -ne 0 ]]; then
+                myNotInstalledPkgs+=("${package}")
+            fi
         fi
+        # 
     done
-    
+    log_lines=0
     apt_str=$(join_by " " ${myNotInstalledPkgs[@]})
     if [[ ${#apt_str} -gt 0 ]]; then
+        
+        if [[ ! -f "${LOG_FILE}" ]]; then
+            touch "${LOG_FILE}"
+        fi
+        
         apt install --simulate $apt_str 2>/dev/null 1>/dev/null
         if [[ $? -eq 0 ]]; then
             apt install $apt_str 2>/dev/null 1>/dev/null
             if [[ $? -eq 0 ]]; then
                 echo "< installed missing packages"
+                echo $(date +"%d.%m.%Y %T.%N")": installed $apt_str" >> ${LOG_FILE}
+                log_lines=$((log_lines+1))
             fi
         else
             echo "not doing anything"
+            ERRORS_COUNT=$((ERRORS_COUNT+1))
         fi
     else
+        echo $(date +"%d.%m.%Y %T.%N")": no packages missing" >> ${LOG_FILE}
+        log_lines=$((log_lines+1))
         echo "< no packages missing"
     fi
+    echo $(date +"%d.%m.%Y %T.%N")": ${ERRORS_COUNT} errors detected" >> ${LOG_FILE}
+    log_lines=$((log_lines+1))
+    
 else
     if [[ $# -gt 0 ]]; then
-    echo $#
+        echo $#
     fi
 fi
     
+if [[ $ACTION_POST_LOG_AFTER -eq 1 ]]; then
+    tail -n $log_lines ${LOG_FILE}
+fi
